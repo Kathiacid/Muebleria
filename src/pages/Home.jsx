d500a86bd095d49734a6dc7e223b190ac2a249e1
kat <kathiacid98@gmail.com>
2025-10-13 21:36:38 -0300

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,12 +43,7 @@ export default function Home() {
         <div>
             <AutoBanner banners={bannerData} autoPlayInterval={4000} />
 
-            <section className="carousel">
-                <h2 className="title">Productos Destacados</h2>
-                <ProductCarousel />
-            </section>
-
-            <section className="grid">
+        <section className="grid">
                 <div className="img1">
                     <img
                         src="https://construmartcl.vtexassets.com/arquivos/ids/214515-800-auto?v=638602983701800000&width=800&height=auto&aspect=true"
@@ -58,41 +53,28 @@ export default function Home() {
                 </div>
 
                 <div className="texto1">
-                    <h2>Tu cocina</h2>
+                    <h2>Tu </h2> 
+                    <h2>cocina</h2>
                     <p>
                         El café de la mañana, las cenas con amigos, los momentos que importan.
                         Diseñemos la cocina que será el corazón de tu hogar.
                     </p>
+                    {/* CORRECCIÓN: El span debe estar DENTRO del Link */}
                     <Link 
                         to={categoriaCocina ? `/catalogo?categoria=${categoriaCocina.id}` : "/catalogo"} 
                         className="mi-boton"
                     >
-                        Ver más
+                        <span className="nav-text">Ver más</span>
+                        <span className="underline-right"></span>
                     </Link>
-                </div>
-
-                <div className="texto2">
-                    <h2>Tu baño</h2>
-                    <p>
-                        Transforma tu baño en un santuario de tranquilidad y orden. Donde cada
-                        mañana comienza con calma y cada noche termina en relax.
-                    </p>
-                    <Link 
-                        to={categoriaBano ? `/catalogo?categoria=${categoriaBano.id}` : "/catalogo"} 
-                        className="mi-boton"
-                    >
-                        Ver más
-                    </Link>
-                </div>
-
-                <div className="img2">
-                    <img
-                        src="https://mkchile.vtexassets.com/arquivos/ids/5278608-800-auto?v=638899139689400000&width=800&height=auto&aspect=true"
-                        className="imagen2"
-                        alt="Baño"
-                    />
                 </div>
             </section>
+
+            <section className="carousel">
+                <h2 className="title">Productos Destacados</h2>
+                <ProductCarousel />
+            </section>
+
         </div>
     );
 }
